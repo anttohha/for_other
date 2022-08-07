74ea93bc0082248765bfdcd68effeb4263473aeb
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-07 08:58:10 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -8,10 +8,11 @@ sudo lxc-attach php -- yum install php -y
 sudo lxc-attach html -- systemctl start httpd
 
 sudo cp index.html /var/lib/lxc/html/rootfs/var/www/html/
+sudo cp index.php /var/lib/lxc/php/rootfs/var/www/html/
 
 iphtml=$(sudo lxc-ls -f | awk '{print $5}' | head -2| tail -1)
 ipphp=$(sudo lxc-ls -f | awk '{print $5}' | tail -1)
 sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to-destination $iphtml
-sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to-destination $ipphp
+sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 81 -j DNAT --to-destination $ipphp
 
 
