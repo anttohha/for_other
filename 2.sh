64f8d4fc4f9d23d6b0be4fde1fbf45241f41a803
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-07 09:52:31 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -5,10 +5,14 @@ sudo lxc-attach html -- systemctl start httpd
 sudo lxc-attach php -- yum update
 sudo lxc-attach php -- yum install httpd -y
 sudo lxc-attach php -- yum install php -y
-sudo lxc-attach html -- systemctl start httpd
+sudo lxc-attach php -- systemctl start httpd
 
 sudo cp index.html /var/lib/lxc/html/rootfs/var/www/html/
 sudo cp index.php /var/lib/lxc/php/rootfs/var/www/html/
+
+sudo sed -i 's/Listen 80/Listen 8081/g' /var/lib/lxc/php/rootfs/etc/httpd/conf/httpd.conf
+sudo lxc-attach php -- systemctl restart httpd
+ 
 
 iphtml=$(sudo lxc-ls -f | awk '{print $5}' | head -2| tail -1)
 ipphp=$(sudo lxc-ls -f | awk '{print $5}' | tail -1)
