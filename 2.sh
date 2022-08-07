2dd423c40d370f3fc5c18ff7cb96161c1079660a
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-07 09:19:40 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -13,6 +13,6 @@ sudo cp index.php /var/lib/lxc/php/rootfs/var/www/html/
 iphtml=$(sudo lxc-ls -f | awk '{print $5}' | head -2| tail -1)
 ipphp=$(sudo lxc-ls -f | awk '{print $5}' | tail -1)
 sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to-destination $iphtml
-sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 81 -j DNAT --to-destination $ipphp
+sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 8081 -j DNAT --to-destination $ipphp
 
 
